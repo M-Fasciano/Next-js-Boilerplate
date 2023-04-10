a987c05f0c4eed6852cd975503d21012e3b44f19
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-04-10 17:12:53 +0100

    feat: card

---
+++ src/components/molecules/Card/Card.style.tsx
@@ -0,0 +1,8 @@
+export const style = {
+  card: `relative flex flex-col border-2 border-gray-200 rounded-lg shadow-sm`,
+  cardImageWrapper: `relative w-full h-48 overflow-hidden rounded-t-lg`,
+  cardImage: `object-cover rounded-t-lg`,
+  cardBody: `block flex-grow flex-shrink p-5`,
+  cardTitle: `font-medium text-gray-700 mb-3`,
+  cardText: `text-gray-500`,
+};
