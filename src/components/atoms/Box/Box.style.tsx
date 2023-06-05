13bbe26eec2149da3c10df706aa883c43b1058ef
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-06-05 08:27:19 +0100

    feat: location

---
+++ src/components/atoms/Box/Box.style.tsx
@@ -0,0 +1,5 @@
+export const style = {
+  border: {
+    default: `border border-gray-400 rounded-lg p-3`,
+  },
+};
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import MapBox from '@/components/atoms/Map';
-import Select from '@/components/atoms/Select';
+import Location from '@/components/molecules/Location';
 import SearchBox from '@/components/molecules/SearchBox';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
@@ -17,12 +17,15 @@ const Map = () => {
   return (
     <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
       <div className="flex items-center justify-between">
-        <SearchBox
-          setResults={setResults}
-          coordinates={coordinates}
-          setCoordinates={setCoordinates}
-        />
-        <Select setRadius={setRadius} />
+        {results.length > 0 ? (
+          <Location results={results} setRadius={setRadius} />
+        ) : (
+          <SearchBox
+            setResults={setResults}
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
+          />
+        )}
       </div>
       <br />
       {results.length > 0 ? <MapBox results={results} radius={radius} /> : null}
