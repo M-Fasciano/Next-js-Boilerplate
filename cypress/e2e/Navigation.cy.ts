bbbf563daf54f96c54c48325c92f74a1f55f0c0e
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-15 06:56:04 +0100

    feat: cypress test

--- cypress/e2e/Navigation.cy.ts
+++ cypress/e2e/Navigation.cy.ts
@@ -5,9 +5,7 @@ describe('Navigation', () => {
       cy.visit('/');
 
       // The index page should contain an h1
-      cy.findByRole('heading', {
-        name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
-      });
+      cy.findByRole('heading');
 
       // Find a link containing "About" text and click it
       cy.findByRole('link', { name: 'About' }).click();
@@ -16,19 +14,14 @@ describe('Navigation', () => {
       cy.url().should('include', '/about');
 
       // The new page should contain two "lorem ipsum" paragraphs
-      cy.findAllByText('Lorem ipsum dolor sit amet', { exact: false }).should(
-        'have.length',
-        2
-      );
+      cy.findByRole('heading');
     });
 
     it('should take screenshot of the homepage', () => {
       cy.visit('/');
 
       // Wait until the page is displayed
-      cy.findByRole('heading', {
-        name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
-      });
+      cy.findByRole('heading');
 
       cy.percySnapshot('Homepage');
     });
