128a5eff6ae91003e671b71102b4a1683a43df0f
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-22 13:48:06 +0200

    feat: trainers

---
+++ src/pages/trainers.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Trainers = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Trainers</h1>
+    </Main>
+  );
+};
+
+export default Trainers;
