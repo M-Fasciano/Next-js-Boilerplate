4c7fcaa83e1c5c7d6984838e6796c8dbddcace93
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-23 16:55:24 +0200

    feat: find a trainer

---
+++ src/pages/find-a-trainer.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const FindATrainer = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Find A Trainer</h1>
+    </Main>
+  );
+};
+
+export default FindATrainer;
