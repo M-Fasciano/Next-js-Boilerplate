7ff4a51bf1c2bbe3131dd18654daca6951de05eb
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-24 10:16:01 +0200

    feat: help and support

---
+++ src/pages/help-and-support.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const HelpAndSupport = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Help And Support</h1>
+    </Main>
+  );
+};
+
+export default HelpAndSupport;
