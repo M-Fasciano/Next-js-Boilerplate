e042f8d9ec8f17fb9078e18daea1d1031f8fa515
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-26 10:15:17 +0200

    feat: terms of use

---
+++ src/pages/terms-of-use.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const TermsOfUse = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Terms of use</h1>
+    </Main>
+  );
+};
+
+export default TermsOfUse;
