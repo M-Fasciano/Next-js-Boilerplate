95a494c4f1df695dc95f583cbc4b02166b6b7e80
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-07-27 11:31:49 +0200

    feat: privacy policy

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const PrivacyPolicy = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Privacy policy</h1>
+    </Main>
+  );
+};
+
+export default PrivacyPolicy;
