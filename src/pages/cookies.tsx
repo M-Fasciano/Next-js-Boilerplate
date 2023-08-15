e0132e0d6dace5dfe9e101c52b820fa682c25ae7
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-08-15 07:34:47 +0100

    feat: cookies page and footer links update

---
+++ src/pages/cookies.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Cookies = () => {
+  return (
+    <Main meta={<Meta title="" description="" />}>
+      <h1 className="text-3xl font-bold">Cookies</h1>
+    </Main>
+  );
+};
+
+export default Cookies;
