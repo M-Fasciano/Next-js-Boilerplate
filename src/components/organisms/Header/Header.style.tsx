c99255e81c071d7370c470cd7cc5d31ffee20b03
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-09-09 12:05:53 +0100

    feat: wip responsive navigation

--- src/components/organisms/Header/Header.style.tsx
+++ src/components/organisms/Header/Header.style.tsx
@@ -1,5 +1,5 @@
 export const style = {
-  default: `flex gap-4 lg:gap-14 items-center`,
+  default: `flex gap-4 lg:gap-14 items-center w-max`,
   color: {
     ligth: `text-white hover:border-white hover:text-white`,
     dark: `text-secondary hover:border-secondary hover:text-secondary`,
