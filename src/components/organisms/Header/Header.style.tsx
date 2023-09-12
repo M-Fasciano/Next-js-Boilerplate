7a988db863f28b8fb3f217041bb34acdbe183269
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-09-12 07:51:07 +0100

    feat: added scroll animation to navigation

---
+++ src/components/organisms/Header/Header.style.tsx
@@ -0,0 +1,9 @@
+export const style = {
+  header: {
+    default:
+      'fixed z-10 flex w-full items-center gap-14 p-4 lg:px-10 lg:py-4 trasition duration-300 ease-in-out',
+    scrolled: 'bg-secondary-light-0',
+    inner:
+      'flex w-full items-center justify-between gap-4 md:justify-start lg:gap-14',
+  },
+};
