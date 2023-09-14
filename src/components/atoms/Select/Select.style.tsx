ee29e3ff032a2d80fc1247842a61e6589e91812e
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-09-14 08:11:46 +0100

    feat: format change

--- src/components/atoms/Select/Select.style.tsx
+++ src/components/atoms/Select/Select.style.tsx
@@ -1,6 +1,6 @@
 export const style = {
   border: {
-    default: `border border-gray-400 rounded-lg p-3 pr-10 appearance-none`,
+    default: 'border border-gray-400 rounded-lg p-3 pr-10 appearance-none',
   },
-  noBorder: `border-none rounded-lg pr-6 ml-3 appearance-none`,
+  noBorder: 'border-none rounded-lg pr-6 ml-3 appearance-none',
 };
