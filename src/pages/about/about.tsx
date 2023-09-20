ef391824ffdc02c16ac44535b11b3aa234d3eef3
michele fasciano <michelefasciano37@hotmail.co.uk>
2023-09-20 08:26:26 +0100

    feat: about renamed

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -5,7 +5,23 @@ import { Main } from '@/templates/Main';
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <h1 className="text-3xl font-bold">About</h1>
-    <div className="-mx-3 flex flex-wrap">
+    <div className="flex flex-wrap md:-mx-3">
+      <div className="mb-6 flex w-full md:mb-0 md:w-6/12 md:p-3">
+        <Card
+          imgSrc="https://picsum.photos/seed/picsum/300/200"
+          title="Card title"
+          text="Proin faucibus arcu quis ante. Aenean commodo ligula eget dolor. Aliquam eu nunc. Sed aliquam ultrices mauris. Fusce vulputate eleifend sapien."
+          cta="View details"
+        />
+      </div>
+      <div className="mb-6 flex w-full md:mb-0 md:w-6/12 md:p-3">
+        <Card
+          imgSrc="https://picsum.photos/seed/picsum/300/200"
+          title="Card title"
+          text="Proin faucibus arcu quis ante. Aenean commodo ligula eget dolor. Aliquam eu nunc. Sed aliquam ultrices mauris. Fusce vulputate eleifend sapien."
+          cta="View details"
+        />
+      </div>
       <div className="mb-6 flex w-full md:mb-0 md:w-6/12 md:p-3">
         <Card
           imgSrc="https://picsum.photos/seed/picsum/300/200"
